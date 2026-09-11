122fce45b8160183ffb68ea9d34ed20d94166788
konstantin <konstantin.klein@hochfrequenz.de>
2026-09-11 08:29:22 +0200

    Fix the FV2404 start date and the template-repository leftover (#146)

--- unittests/test_edifact_format_version.py
+++ unittests/test_edifact_format_version.py
@@ -35,7 +35,7 @@ from efoli.edifact_format_version import _format_version_thresholds, _latest_for
         pytest.param(datetime(2024, 1, 1, 0, 0, 0, tzinfo=timezone.utc), EdifactFormatVersion.FV2310),
         pytest.param(
             datetime(2024, 4, 1, 0, 0, 0, tzinfo=timezone.utc), EdifactFormatVersion.FV2310
-        ),  # 2404 is valid form 2024-04-03 onwards
+        ),  # 2404 is valid from 2024-04-03 onwards
         pytest.param(datetime(2024, 4, 2, 22, 0, 0, tzinfo=timezone.utc), EdifactFormatVersion.FV2404),
         pytest.param(datetime(2024, 9, 30, 21, 59, 59, tzinfo=timezone.utc), EdifactFormatVersion.FV2404),
         pytest.param(datetime(2024, 9, 30, 22, 0, 0, 0, tzinfo=timezone.utc), EdifactFormatVersion.FV2410),
